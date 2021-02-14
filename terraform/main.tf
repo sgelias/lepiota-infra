4c5792de39a1500a48b9ae518a54197ca5ae88d7
Samuel Galvão Elias <samuel.elias@neoprospecta.com>
2021-02-14 12:45:57 -0300

    Initial commit.

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.0"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "lepiota-tfstate"
+    key    = "terraformt.tfstate"
+    region = "us-east-1"
+  }
+}
