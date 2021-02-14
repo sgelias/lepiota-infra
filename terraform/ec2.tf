abcc68bc8cd4c0fdf8dadeb05d3f4625fc5b80dc
Samuel Galvão Elias <samuel.elias@neoprospecta.com>
2021-02-14 15:44:54 -0300

    Set publick key pair hard coded.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "web" {
   key_name   = "lepista-iaas"
-  public_key = file("../.secrets/lepista-iaas.pub")
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDzywQTmWxvTN0xWRj0t3XybYtQUVZhSUhAknM4AoVYfCq5WI4A2zq/sPTDLFTbYUlF5148XBU5PqTp9FbI/ZWFLZRqX6ur/pawzIScpsTTAv+hb0NKSd5diHZkIavAl8FgCix67gvYbfHdwgcplhfRRxiaRjEXLH9t/1ON0QR0DpmEGgLkKm0WSpdnEZKJdVmmNlQ6Zw+Kblwk0oxwrfk10DvXdjnt6xPebVOa/04s6wUHF0TGFkdOTvtOqOjOJHqKpoKSttRdzVxQ/RHAT2JXILKoAO3jhbbIWLfpx5Gp1F7unJXr27uZfZan8ALhV9iR9RfoCQSIfvWDRQJpaWwWetT+5XbZST3oxGIZy9zc1LZVUvnFb6lnSE8VQskH8wLBJlSHhkzRMwQabemERADqwNLe6oMwGP8cgbXSNSlJf/hhpidQllWc809Ksa4I9SYZoHmskqgMKGclk0bF3SnyuK3Q7c7ZVhREPPQX2IH2QXy5LYdM1VZxQ5ylKM+UchE= root@33242dfd1a6e"
 }
 
 resource "aws_instance" "web" {
