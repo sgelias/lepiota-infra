8eeb5ffa15e2d1e1a896ae8828b88bb6aa032697
Samuel Galvão Elias <samuel.elias@neoprospecta.com>
2021-02-15 00:32:21 -0300

    Include cidr_block specifity 3.

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "private" {
 
 resource "aws_subnet" "public" {
   vpc_id                      = aws_vpc.main.id
-  cidr_block                  = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+  cidr_block                  = cidrsubnet(aws_vpc.main.cidr_block, 8, 4)
   map_public_ip_on_launch     = true
 
   tags = {
