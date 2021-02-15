e45d6a28416a26867db205a75a88dea6d5a69979
Samuel Galvão Elias <samuel.elias@neoprospecta.com>
2021-02-14 22:02:37 -0300

    Update security group and create output in terraform.

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -3,20 +3,20 @@ resource "aws_security_group" "ssh_access" {
   description = "Allow http access"
   vpc_id      = aws_vpc.main.id
 
-  ingress = [{
+  ingress = {
     description = "ssh_access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [aws_vpc.main.cidr_block]
-  }]
+  }
 
-  egress = [{
+  egress = {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }]
+  }
 
   tags = {
     Name = "ssh_access"
