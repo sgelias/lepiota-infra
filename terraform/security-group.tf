18ab52faaf86fa61c22fb39c08e126152a889739
Samuel Galvão Elias <samuel.elias@neoprospecta.com>
2021-02-14 21:09:22 -0300

    Create the custom security group for the cluster.

---
+++ terraform/security-group.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "allow_http" {
+  name        = "lepista-k8s-${var.hash_commit}"
+  description = "Allow http access"
+  vpc_id      = "value"
+
+  ingress = {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress = {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc" "main" {
+  id                   = "lepista-k8s-${var.hash_commit}"
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
 
