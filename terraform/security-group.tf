f8524c57e5ea1365ef8113e888b1b850e4b09629
Samuel Galvão Elias <samuel.elias@neoprospecta.com>
2021-02-14 22:15:35 -0300

    Fix bug security group to add empty paramaters.

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -4,26 +4,26 @@ resource "aws_security_group" "ssh_access" {
   vpc_id      = aws_vpc.main.id
 
   ingress = [{
-    description = "ssh_access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks: null
-    prefix_list_ids: null
-    security_groups: null
-    self: null
+    description      = "ssh_access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = null
+    prefix_list_ids  = null
+    security_groups  = null
+    self             = null
   }]
 
   egress = [{
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks: null
-    prefix_list_ids: null
-    security_groups: null
-    self: null
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = null
+    prefix_list_ids  = null
+    security_groups  = null
+    self             = null
   }]
 
   tags = {
